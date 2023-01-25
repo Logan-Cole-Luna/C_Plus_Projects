fb124ea7bb350c58435779d69ffa1773ef1c00c5
loganlambeth <loganlambeth8@gmail.com>
2023-01-24 19:46:16 -0500

    First Files

---
+++ SurfaceAreaCalculator.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Logan Lambeth on 1/24/23.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "This program will calculate the surface area of a package, with the dimensions provided by the user." << std::endl;
+    return 0;
+}
